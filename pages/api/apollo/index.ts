7c4f0843093b3e4a3a2e2dc14dfd198c90cdabcb
Andrew Diedrich <diedr067@gmail.com>
2023-04-26 15:05:11 -0500

    no cores

--- pages/api/apollo/index.ts
+++ pages/api/apollo/index.ts
@@ -7,6 +7,7 @@ const authLink = new ApolloLink((operation, forward) => {
       "Access-Control-Allow-Origin": "*", // Required for CORS support to work
       "Access-Control-Allow-Credentials": true, // Required for cookies, authorization headers with HTTPS
       "X-TENSOR-API-KEY": process.env.NEXT_PUBLIC_TENSOR_API_KEY,
+      mode: "no-cors",
     },
   });
   return forward(operation);
